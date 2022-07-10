5d637bbf3678839d0bc5f7335a4888cf68cc02f7
cintamg <cintaelamthottam@gmail.com>
2022-07-11 01:49:38 +0530

    chrome extension

---
+++ chrome_ext/chrome_ext.js
@@ -0,0 +1,7 @@
+const myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-el");
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked executed from addEventListener");
+})
