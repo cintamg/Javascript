9f75d6bb8d921c77d90a1bf79c862c072a0556dc
cintamg <cintaelamthottam@gmail.com>
2022-07-19 13:59:09 +0530

    changes

--- chrome_ext/chrome_ext.js
+++ chrome_ext/chrome_ext.js
@@ -1,7 +1,24 @@
-const myLeads = [];
+let myLeads = []
 const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
+let i=0
+let listItem = " "
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked executed from addEventListener");
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
+function renderLeads(){
+    let listItem = " "
+    for(let i=0;i<myLeads.length;i++){
+        listItem += 
+        `<li>
+          <a target='_blank' href='${myLeads[i]}'>
+             ${myLeads[i]}
+          </a>
+        </li>`
+    }
+    ulEl.innerHTML = listItem
+}
